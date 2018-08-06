52001162355d60f68dcf63f08dd20c3cb7b0f6e4
bexi <trreitmaier@msn.com>
2018-08-06 23:09:38 +0200

    added service which checks if a user is authed

--- server/router.js
+++ server/router.js
@@ -4,10 +4,12 @@ const passport = require('passport');
 
 // session false --> no cookie
 const requireAuth = passport.authenticate('jwt', {session: false});
+const requireSignin = passport.authenticate('local', {session: false});
 
 module.exports = function(app){
   app.get('/', requireAuth, function(req, res){
     res.send({hi: 'hi'});
-  })
+  });
+  app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
 }
