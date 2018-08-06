40e2a0fdc238b7c691a21e1b795c0345bfa5dd82
bexi <trreitmaier@msn.com>
2018-08-06 23:08:50 +0200

    created compare method for passwords

--- server/models/user.js
+++ server/models/user.js
@@ -30,6 +30,14 @@ userSchema.pre('save', function(next) {
   });
 });
 
+// This is used in passport.js local strategy
+userSchema.methods.comparePassword = function(candidatePassword, callback){
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+    if(err) {return callback(err)}
+    callback(null, isMatch);
+  })
+}
+
 // create the model class
 const ModelClass = mongoose.model('user', userSchema);
 
